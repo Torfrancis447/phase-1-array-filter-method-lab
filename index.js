6f9a6ad6937aa0e54ac239c0ef7320fbff6dc5cb
tor <torfrancis447@gmail.com>
2022-11-05 18:28:01 -0400

    commit tree 6a6aeb0ed98d6e8caa4167420f4c0bbe7f704942

--- index.js
+++ index.js
@@ -1,1 +1,38 @@
 // Code your solution here
+const drivers1 = ['Bobby', 'Sammy', 'Sally', 'Annette', 'Sarah', 'Bobby']
+function findMatching(drivers, input) {
+    
+    const results = drivers.filter(driver => driver.toLowerCase() === input.toLowerCase())
+    return (results)
+}
+
+function fuzzyMatch(drivers, input) {
+const results = drivers.filter(driver => driver[0] === input[0] )
+return (results)
+}
+
+
+const drivers = [
+    {
+      name: 'Bobby',
+      hometown: 'Pittsburgh' },
+    {
+      name: 'Sammy',
+      hometown: 'New York' } ,
+    {
+      name: 'Sally',
+      hometown: 'Cleveland' },
+    {
+      name: 'Annette',
+      hometown: 'Los Angeles' },
+    {
+      name: 'Bobby',
+      hometown: 'Tampa Bay' }
+  ];
+
+function matchName(drivers, input) {
+const results = drivers.filter(driver => driver.name === input)
+return (results)
+}
+
+matchName(drivers, "Bobby")
